b8c7b7ce3c4ba618c918034c5b1868996ed9b93f
Pranav C <xtreme5351@gmail.com>
2026-07-29 01:08:05 +0100

    [ADD] added sub op support + fixes

--- include/operations.h
+++ include/operations.h
@@ -18,7 +18,12 @@ TensorOne& operator+=(double k, TensorOne& a);
 TensorOne& operator+=(TensorOne& a, const TensorOne& b);
 
 // subtraction overloads
+TensorOne operator-(const TensorOne& a, double k);
+TensorOne operator-(double k, const TensorOne& a);
 TensorOne operator-(const TensorOne& a, const TensorOne& b);
+TensorOne& operator-=(TensorOne& a, double k);
+TensorOne& operator-=(double k, TensorOne& a);
+TensorOne& operator-=(TensorOne& a, const TensorOne& b);
 
 // multiplication overloads
 TensorOne operator*(const TensorOne& a, const TensorOne& b);
--- src/ops/subtraction.cpp
+++ src/ops/subtraction.cpp
@@ -19,4 +19,84 @@ TensorOne operator-(const TensorOne& a, const TensorOne& b) {
   return TensorOne(a.tape, new_id);
 }
 
+TensorOne operator-(const TensorOne& a, const double k) {
+  const size_t n = a.size();
+
+  if (a.tape == nullptr) {
+    std::vector<double> res(n);
+    for (size_t i = 0; i < n; ++i) res[i] = a.data[i] - k;
+    return TensorOne(res, false, nullptr);
+  }
+  const TensorOne b(n, k, false, a.tape);
+  const size_t new_id = a.tape->sub_one(a.node_id, b.node_id);
+  return TensorOne(a.tape, new_id);
+}
+
+TensorOne operator-(const double k, const TensorOne& a) {
+  const size_t n = a.size();
+
+  if (a.tape == nullptr) {
+    std::vector<double> res(n);
+    for (size_t i = 0; i < n; ++i) res[i] = k - a.data[i];
+    return TensorOne(res, false, nullptr);
+  }
+
+  const TensorOne b(n, k, false, a.tape);
+  const size_t new_id = a.tape->sub_one(b.node_id, a.node_id);
+  return TensorOne(a.tape, new_id);
+}
+
+TensorOne& operator-=(TensorOne& a, const double k) {
+  const size_t n = a.size();
+
+  if (a.tape == nullptr) {
+    for (size_t i = 0; i < n; ++i) a.data[i] -= k;
+    return a;
+  }
+
+  std::vector<double> scalar_value(n, k);
+  const size_t scalar_id = a.tape->add_node_one(std::move(scalar_value), false);
+  const size_t new_id = a.tape->sub_one(a.node_id, scalar_id);
+
+  a.data = a.tape->values_one[new_id];
+  a.node_id = new_id;
+  a.requires_grad = a.tape->required_grads[new_id];
+  return a;
+}
+
+TensorOne& operator-=(TensorOne& a, const TensorOne& b) {
+  if (a.tape != b.tape) {
+    throw std::invalid_argument("Tensors must have the same tape");
+  }
+  if (a.tape == nullptr) {
+    for (size_t i = 0; i < a.size(); ++i) a.data[i] -= b.data[i];
+    return a;
+  }
+
+  const size_t new_id = a.tape->sub_one(a.node_id, b.node_id);
+  a.data = a.tape->values_one[new_id];
+  a.node_id = new_id;
+  a.requires_grad = a.tape->required_grads[new_id];
+  return a;
+}
+
+TensorOne& operator-=(const double k, TensorOne& a) {
+  const size_t n = a.size();
+
+  if (a.tape == nullptr) {
+    for (size_t i = 0; i < n; ++i) a.data[i] = k - a.data[i];
+    return a;
+  }
+
+  std::vector<double> scalar_value(n, k);
+  const size_t scalar_id = a.tape->add_node_one(std::move(scalar_value), false);
+  const size_t new_id =
+      a.tape->sub_one(scalar_id, a.node_id);  // k - a, not a - k
+
+  a.data = a.tape->values_one[new_id];
+  a.node_id = new_id;
+  a.requires_grad = a.tape->required_grads[new_id];
+  return a;
+}
+
 }  // namespace autograd
